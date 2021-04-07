6e33f01793e67b2b6fa072026aff1ed1105cf99a
Ichsanul Kamil Sudarmi <ichkmlsdr@gmail.com>
2021-04-07 21:31:56 +0700

    add color

--- PA_CGA/Form1.Designer.vb
+++ PA_CGA/Form1.Designer.vb
@@ -23,16 +23,23 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
-        Me.Canvas = New System.Windows.Forms.PictureBox()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.rbHollow = New System.Windows.Forms.RadioButton()
         Me.rbFill = New System.Windows.Forms.RadioButton()
+        Me.rbHollow = New System.Windows.Forms.RadioButton()
+        Me.Canvas = New System.Windows.Forms.PictureBox()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.ColorDialog2 = New System.Windows.Forms.ColorDialog()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.rb_Blue = New System.Windows.Forms.RadioButton()
+        Me.rb_Red = New System.Windows.Forms.RadioButton()
+        Me.rb_Green = New System.Windows.Forms.RadioButton()
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SplitContainer1.Panel1.SuspendLayout()
         Me.SplitContainer1.Panel2.SuspendLayout()
         Me.SplitContainer1.SuspendLayout()
-        CType(Me.Canvas, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox1.SuspendLayout()
+        CType(Me.Canvas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'SplitContainer1
@@ -43,6 +50,7 @@ Partial Class Form1
         '
         'SplitContainer1.Panel1
         '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.GroupBox2)
         Me.SplitContainer1.Panel1.Controls.Add(Me.GroupBox1)
         '
         'SplitContainer1.Panel2
@@ -51,15 +59,6 @@ Partial Class Form1
         Me.SplitContainer1.Size = New System.Drawing.Size(878, 431)
         Me.SplitContainer1.SplitterDistance = 205
         Me.SplitContainer1.TabIndex = 0
-        '
-        'Canvas
-        '
-        Me.Canvas.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Canvas.Location = New System.Drawing.Point(0, 0)
-        Me.Canvas.Name = "Canvas"
-        Me.Canvas.Size = New System.Drawing.Size(669, 431)
-        Me.Canvas.TabIndex = 0
-        Me.Canvas.TabStop = False
         '
         'GroupBox1
         '
@@ -72,6 +71,18 @@ Partial Class Form1
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Options"
         '
+        'rbFill
+        '
+        Me.rbFill.AutoSize = True
+        Me.rbFill.Checked = True
+        Me.rbFill.Location = New System.Drawing.Point(13, 47)
+        Me.rbFill.Name = "rbFill"
+        Me.rbFill.Size = New System.Drawing.Size(49, 17)
+        Me.rbFill.TabIndex = 2
+        Me.rbFill.TabStop = True
+        Me.rbFill.Text = "Filled"
+        Me.rbFill.UseVisualStyleBackColor = True
+        '
         'rbHollow
         '
         Me.rbHollow.AutoSize = True
@@ -79,19 +90,64 @@ Partial Class Form1
         Me.rbHollow.Name = "rbHollow"
         Me.rbHollow.Size = New System.Drawing.Size(57, 17)
         Me.rbHollow.TabIndex = 1
-        Me.rbHollow.TabStop = True
         Me.rbHollow.Text = "Hollow"
         Me.rbHollow.UseVisualStyleBackColor = True
         '
-        'rbFill
+        'Canvas
         '
-        Me.rbFill.AutoSize = True
-        Me.rbFill.Location = New System.Drawing.Point(13, 47)
-        Me.rbFill.Name = "rbFill"
-        Me.rbFill.Size = New System.Drawing.Size(39, 14)
-        Me.rbFill.TabIndex = 2
-        Me.rbFill.Text = "Filled"
-        Me.rbFill.UseVisualStyleBackColor = True
+        Me.Canvas.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Canvas.Location = New System.Drawing.Point(0, 0)
+        Me.Canvas.Name = "Canvas"
+        Me.Canvas.Size = New System.Drawing.Size(669, 431)
+        Me.Canvas.TabIndex = 0
+        Me.Canvas.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.rb_Green)
+        Me.GroupBox2.Controls.Add(Me.rb_Blue)
+        Me.GroupBox2.Controls.Add(Me.rb_Red)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 96)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(178, 78)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Options"
+        '
+        'rb_Blue
+        '
+        Me.rb_Blue.AutoSize = True
+        Me.rb_Blue.BackColor = System.Drawing.Color.Blue
+        Me.rb_Blue.Location = New System.Drawing.Point(13, 47)
+        Me.rb_Blue.Name = "rb_Blue"
+        Me.rb_Blue.Size = New System.Drawing.Size(46, 17)
+        Me.rb_Blue.TabIndex = 2
+        Me.rb_Blue.Text = "Blue"
+        Me.rb_Blue.UseVisualStyleBackColor = False
+        '
+        'rb_Red
+        '
+        Me.rb_Red.AutoSize = True
+        Me.rb_Red.BackColor = System.Drawing.Color.Red
+        Me.rb_Red.Checked = True
+        Me.rb_Red.Location = New System.Drawing.Point(13, 21)
+        Me.rb_Red.Name = "rb_Red"
+        Me.rb_Red.Size = New System.Drawing.Size(45, 17)
+        Me.rb_Red.TabIndex = 1
+        Me.rb_Red.TabStop = True
+        Me.rb_Red.Text = "Red"
+        Me.rb_Red.UseVisualStyleBackColor = False
+        '
+        'rb_Green
+        '
+        Me.rb_Green.AutoSize = True
+        Me.rb_Green.BackColor = System.Drawing.Color.Lime
+        Me.rb_Green.Location = New System.Drawing.Point(105, 21)
+        Me.rb_Green.Name = "rb_Green"
+        Me.rb_Green.Size = New System.Drawing.Size(54, 17)
+        Me.rb_Green.TabIndex = 3
+        Me.rb_Green.Text = "Green"
+        Me.rb_Green.UseVisualStyleBackColor = False
         '
         'Form1
         '
@@ -105,9 +161,11 @@ Partial Class Form1
         Me.SplitContainer1.Panel2.ResumeLayout(False)
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.SplitContainer1.ResumeLayout(False)
-        CType(Me.Canvas, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        CType(Me.Canvas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -117,4 +175,10 @@ Partial Class Form1
     Friend WithEvents rbFill As RadioButton
     Friend WithEvents rbHollow As RadioButton
     Friend WithEvents Canvas As PictureBox
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents rb_Green As RadioButton
+    Friend WithEvents rb_Blue As RadioButton
+    Friend WithEvents rb_Red As RadioButton
+    Friend WithEvents ColorDialog2 As ColorDialog
 End Class
--- PA_CGA/Form1.vb
+++ PA_CGA/Form1.vb
@@ -27,15 +27,38 @@ Public Class Form1
         Canvas.Invalidate()
     End Sub
 
+    Dim brush_color As Brush
+    Private Sub rb_Red_CheckedChanged(sender As Object, e As EventArgs) Handles rb_Red.CheckedChanged
+        If rb_Red.Checked = True Then
+            rb_Blue.Checked = False
+            rb_Green.Checked = False
+            brush_color = Brushes.Red
+        End If
+    End Sub
+
+    Private Sub rb_Green_CheckedChanged(sender As Object, e As EventArgs) Handles rb_Green.CheckedChanged
+        If rb_Green.Checked = True Then
+            rb_Blue.Checked = False
+            rb_Red.Checked = False
+            brush_color = Brushes.Green
+        End If
+    End Sub
+
+    Private Sub rb_Blue_CheckedChanged(sender As Object, e As EventArgs) Handles rb_Blue.CheckedChanged
+        If rb_Blue.Checked = True Then
+            rb_Red.Checked = False
+            rb_Green.Checked = False
+            brush_color = Brushes.Blue
+        End If
+    End Sub
     Private Sub Canvas_Paint(sender As Object, e As PaintEventArgs) Handles Canvas.Paint
         e.Graphics.SmoothingMode = SmoothingMode.AntiAlias
         e.Graphics.Clear(Canvas.BackColor)
-
         For Each polygon As List(Of Point) In polygons
             If rbFill.Checked = True Then
-                e.Graphics.FillPolygon(Brushes.White, polygon.ToArray)
+                e.Graphics.FillPolygon(brush_color, polygon.ToArray)
             End If
-            e.Graphics.DrawPolygon(Pens.Black, polygon.ToArray)
+            e.Graphics.DrawPolygon(Pens.Transparent, polygon.ToArray)
         Next polygon
 
         If newpolygon IsNot Nothing Then
@@ -65,6 +88,4 @@ Public Class Form1
             rbHollow.Checked = False
         End If
     End Sub
-
-
 End Class
